de7a7b81f439a05d0be2dcbb761e3bebac42cbac
Tkariclook <tkariclook@gmail.com>
2021-04-02 16:39:34 +0300

    essential-slick-code chapter1
    make exercise and read same theory

--- chapter-01/src/main/scala/main.scala
+++ chapter-01/src/main/scala/main.scala
@@ -3,7 +3,6 @@ import slick.jdbc.H2Profile.api._
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
 
 object Example extends App {
 
@@ -18,7 +17,8 @@ object Example extends App {
     Message("Dave", "Hello, HAL. Do you read me, HAL?"),
     Message("HAL",  "Affirmative, Dave. I read you."),
     Message("Dave", "Open the pod bay doors, HAL."),
-    Message("HAL",  "I'm sorry, Dave. I'm afraid I can't do that.")
+    Message("HAL",  "I'm sorry, Dave. I'm afraid I can't do that."),
+    Example.Message("Mike", "Slick it's cool")
   )
 
   // Schema for the "message" table:
@@ -33,10 +33,27 @@ object Example extends App {
   }
 
   // Base query for querying the messages table:
-  lazy val messages = TableQuery[MessageTable]
+  lazy val  messages = TableQuery[MessageTable]
+  // messages: TableQuery[MessageTable] = Rep(TableExpansion)
 
   // An example query that selects a subset of messages:
   val halSays = messages.filter(_.sender === "HAL")
+
+  val halSays2 = for {
+    message <- messages if message.sender === "HAL"
+  } yield message
+
+  val actions: DBIO[Seq[Message]] = (
+      messages.schema.create andThen
+          (messages ++= freshTestData) andThen
+          halSays.result
+      )
+  val sameActions: DBIO[Seq[Message]] = (
+      messages.schema.create >>
+          (messages ++= freshTestData) >>
+          halSays.result
+      )
+
 
   // Create an in-memory H2 database;
   val db = Database.forConfig("chapter01")
@@ -47,6 +64,10 @@ object Example extends App {
   // Create the "messages" table:
   println("Creating database table")
   exec(messages.schema.create)
+//  messages.schema.createStatements.mkString
+//  val action: DBIO[Unit] = messages.schema.create
+//  val future: Future[Unit] = db.run(action)
+//  val result = Await.result(future, 2.seconds)
 
   // Create and insert the test data:
   println("\nInserting test data")
@@ -57,5 +78,12 @@ object Example extends App {
   exec( messages.result ) foreach { println }
 
   println("\nSelecting only messages from HAL:")
-  exec( halSays.result ) foreach { println }
+  exec( halSays.result ) foreach { str => println(str) }
+  exec( halSays2.result ) foreach { str => println(str) }
+
+  println("111")
+
+
+//  val insert: DBIO[Option[Int]] = messages ++= freshTestData
+//  val insertAction: Future[Option[Int]] = db.run(insert)
 }
