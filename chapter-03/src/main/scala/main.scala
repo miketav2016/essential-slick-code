17dccda2e680a95432b491c3be9508e805e4fbf8
Tkariclook <tkariclook@gmail.com>
2021-04-07 22:36:16 +0300

    essential-slick-code chapter3
    exercises

--- chapter-03/src/main/scala/main.scala
+++ chapter-03/src/main/scala/main.scala
@@ -1,8 +1,8 @@
-import scala.concurrent.Await
-import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
 import slick.jdbc.H2Profile.api._
-import scala.util.Try
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 object Example extends App {
 
@@ -34,16 +34,64 @@ object Example extends App {
     Message("Dave", "Open the pod bay doors, HAL."),
     Message("HAL",  "I'm sorry, Dave. I'm afraid I can't do that."))
 
-  def populate: DBIOAction[Option[Int], NoStream,Effect.All] =  {
-    for {    
-    //Drop table if it already exists, then create the table:
-    _     <- messages.schema.drop.asTry andThen messages.schema.create
-    // Add some data:
-    count <- messages ++= testData
-  } yield count
-    
-    
+  def populate: DBIOAction[Option[Int], NoStream, Effect.All] = {
+    for {
+      //Drop table if it already exists, then create the table:
+      _ <- messages.schema.drop.asTry andThen messages.schema.create
+      // Add some data:
+      count <- messages ++= testData
+    } yield count
+
+
   }
+
+  println("Get to the Specifics")
+  exec(messages.map(m => (m.sender , m.content)) += ("HAL", "content"))
+  println("Bulk All the Inserts")
+  val conversation = List(
+    Message("Bob", "Hi Alice"),
+    Message("Alice", "Hi Bob"),
+    Message("Bob", "Are you sure this is secure?"),
+    Message("Alice", "Totally, why do you ask?"),
+    Message("Bob", "Oh, nothing, just wondering."),
+    Message("Alice", "Ten was too many messages"),
+    Message("Bob", "I could do with a sleep"),
+    Message("Alice", "Let's just to to the point"),
+    Message("Bob", "Okay okay, no need to be tetchy."),
+    Message("Alice", "Humph!"))
+  val messagesReturningRow = messages returning messages.map(_.id) into { (message, id) =>
+    message.copy(id = id)
+  }
+  exec(messagesReturningRow ++= conversation).foreach(println)
+  println("Solution to: No Apologies")
+  val deletSo = messages.filter(_.content.toLowerCase like "%sorry%").delete
+  exec(deletSo)
+  println("Update Using a For Comprehension")
+  val rebootLoop = messages.
+      filter(_.sender === "HAL").
+      map(msg => (msg.sender, msg.content)).
+      update(("HAL 9000", "Rebooting, please wait..."))
+
+  val toUpDate = for {
+    m <- messages if m.sender === "HAL"
+  } yield (m.sender, m.content)
+  val updateAction = toUpDate.update(("HAL 9000", "Rebooting, please wait..."))
+  println("Selective Memory")
+  val dropTwo = messages.filter {
+    _.id in messages
+        .filter(_.sender === "HAL")
+        .sortBy(_.id.asc)
+        .map(_.id)
+        .take(2)
+  }.delete
+  dropTwo.statements.head
+
+
+
+
+
+
+
 
   // Utility to print out what is in the database:
   def printCurrentDatabaseState() = {
